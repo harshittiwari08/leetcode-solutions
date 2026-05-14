b07017e5cfcd8e19990eec103825c9203874862c
Harshit Tiwari <162500512+harshittiwari08@users.noreply.github.com>
2026-05-14 23:07:10 +0530

    Time: 33 ms (83.45%) | Memory: 119 MB (47.41%) - LeetSync

---
+++ 4301-minimize-array-sum-using-divisible-replacements/minimize-array-sum-using-divisible-replacements.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long minArraySum(int[] nums) {
+        Arrays.sort(nums);
+        int max = nums[0];
+        long sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i]>max)
+                max = nums[i];
+        }
+        int[] sieve = new int[max+1];
+        for(int num : nums){
+            sieve[num] += 1;
+        }
+        for(int i = 0; i<=max; i++){
+            if(sieve[i] == 0)
+                continue;
+            for(int j = i; j<= max; j+=i){
+                if(sieve[j] > 0){
+                    sum+=1L* sieve[j]*i;
+                    sieve[j] = 0;
+                }
+            }
+        }
+        return sum;
+    }
+}
