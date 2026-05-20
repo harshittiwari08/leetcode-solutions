f2ee4195ca6b6e0144ffd928b3b733bfbe9cced6
Harshit Tiwari <162500512+harshittiwari08@users.noreply.github.com>
2026-05-20 23:07:48 +0530

    Time: 9 ms (23.30%) | Memory: 47.5 MB (31.21%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
@@ -4,10 +4,7 @@ class Solution {
         int[] ans = new int[A.length];
         int count = 0;
         for(int i=0; i<A.length; i++){
-            // if(set.containsKey(A[i]))
-            //     set.put(A[i],set.get(A[i])+1);
-            // else
-                set.put(A[i],1);          
+            set.put(A[i],1);          
             for(int j =0; j<=i; j++){
                 if(set.containsKey(B[j])){
                     count++;
